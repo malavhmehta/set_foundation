fbfd4d9516a721c55d4a68a410b6515e1aed7813
malav-mehta <malav.mehta@hotmail.com>
2020-08-06 18:55:05 -0400

    finished load animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,47 @@
 import React, {Component} from "react";
 import {BrowserRouter as Router, Route, useLocation} from "react-router-dom";
+import styled, {keyframes} from "styled-components";
 import Switch from "react-router-transition-switch";
 import Fader from "react-fader";
 
-import {GlobalStyle, media} from "../styles";
+import {GlobalStyle, theme, media} from "../styles";
 import {global} from "../data";
 
 import {Nav} from "../components/common/Nav";
 import {ErrorPage} from "./404";
+
+const {colors, fontSizes} = theme;
+
+const SlideIn = keyframes`
+  from {
+    transform: translateY(100%);
+  }
+  
+  to {
+    transform: translateY(0);
+  }
+`
+
+const Loader = styled.div`
+  animation: .75s ${SlideIn} ease-out forwards;
+  align-items: center;
+  background-color: ${colors.bg};
+  display: flex;
+  font-size: ${fontSizes.h1};
+  height: 100vh;
+  justify-content: center;
+  transform: translateY(-100%);
+  width: 100vw;
+  
+  h1 {
+    color: ${colors.accent};
+    font-size: 42px;
+    ${media.sm`font-size: 60px;`};
+    ${media.md`font-size: 80px;`};
+    font-weight: 600;
+    transition: ${theme.transition};  
+  }
+`;
 
 function CurrentRoute({listen, children}) {
   let route = useLocation();
@@ -18,7 +52,7 @@ class App extends Component {
   state = {
     animationDone: false,
     dataFetchDone: false,
-    animationLength: 0
+    animationLength: 750
   };
 
   finishAnimation = () => {
@@ -90,9 +124,11 @@ class App extends Component {
 
     else {
       return (
-          <div className="loader">
-            <h1>SET.Foundation</h1>
-          </div>
+          <React.Fragment>
+            <Loader>
+              <h1>SET.Foundation</h1>
+            </Loader>
+          </React.Fragment>
       );
     }
   }
