545c1b5662bc988fc94ad69c50d37a5510f7cdbd
malav-mehta <malav.mehta@hotmail.com>
2020-08-18 18:34:13 -0400

    added sponsor images

--- src/data/conference.jsx
+++ src/data/conference.jsx
@@ -1,3 +1,12 @@
+import Password from "../assets/images/1password.png";
+import Balsamiq from "../assets/images/balsamiq.png";
+import Carleton from "../assets/images/carleton.png";
+import Huawei from "../assets/images/huawei.png";
+import Repl from "../assets/images/repl.png";
+import Rogers from "../assets/images/rogers.png";
+import UOttawa from "../assets/images/uottawa.jpg";
+import wolfram from "../assets/images/wolfram.png";
+
 export const conference = {
   hero: {
     title: {
@@ -7,4 +16,14 @@ export const conference = {
     lead:
       "Join like-minded high school students interested in pursuing a career in engineering and technology at SET 2020.",
   },
+  sponsors: [
+    Password,
+    Balsamiq,
+    Carleton,
+    Huawei,
+    Repl,
+    Rogers,
+    UOttawa,
+    wolfram,
+  ],
 };
--- src/pages/Conference.jsx
+++ src/pages/Conference.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-import { Landing } from "../components/conference";
+import { Landing, Sponsors } from "../components/conference";
 import { Anchor } from "../components/common";
 
 export const Conference = (props) => (
   <div id="conference">
     <Landing data={props.data.hero} />
+    <Sponsors data={props.data.sponsors} />
     <Anchor anchor="about" />
     <Anchor anchor="why" />
     <Anchor anchor="schedule" />
