0861d1e2101bd9ac8dd8c137c79156a1f4036b1c
malav-mehta <malav.mehta@hotmail.com>
2020-10-10 11:31:35 -0400

    added direct link to SET UP

--- src/pages/Up.jsx
+++ src/pages/Up.jsx
@@ -1,51 +1,164 @@
 import { About, Landing } from "../components/conference";
 import { Anchor, Countdown } from "../components/common";
 import { Form, Schedule, Speakers } from "../components/up";
-import { hex2rgba, theme } from "../styles";
+import { hex2rgba, media, theme } from "../styles";
+import styled, { keyframes } from "styled-components";
 
 import React from "react";
-import styled from "styled-components";
 
 const Line = styled.hr`
   border-top: 1px solid ${hex2rgba(theme.colors.bg_alt, 0.1)};
   margin: 0;
 `;
 
-export const Up = (props) => (
-  <div id="up">
-    <Landing data={props.data.hero} />
-    <Line />
+const { colors, fontSizes } = theme;
 
-    <Anchor anchor="about" />
-    <About data={props.data.about} />
-    <Countdown
-      target={props.data.countdown.time}
-      location={props.data.countdown.location}
-    />
+const ZoomIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  
+  to {
+    opacity: 1;
+  }
+`;
 
-    <div
-      className="container"
-      style={{ marginTop: "8rem", marginBottom: "6rem" }}
-    >
+const Loader = styled.div`
+  animation: 0.5s ${ZoomIn} ease-out forwards;
+  align-items: center;
+  background-color: ${colors.bg};
+  display: flex;
+  flex-direction: column;
+  font-size: ${fontSizes.h1};
+  height: 100vh;
+  justify-content: center;
+  width: 100vw;
+
+  h1 {
+    color: ${colors.accent};
+    font-size: 42px;
+    ${media.sm`font-size: 50px;`};
+    ${media.md`font-size: 65px;`};
+    font-weight: 700;
+    transition: ${theme.transition};
+  }
+`;
+
+const BackButton = styled.button`
+  background-color: ${colors.accent};
+  border: none;
+  border-radius: ${theme.borderRadius};
+  color: ${colors.text_alt} !important;
+  font-size: ${fontSizes.sm};
+  font-weight: 600;
+  padding: 0.8rem 1.4rem;
+  margin-right: 1rem;
+  transition: ${theme.transition};
+
+  &:hover {
+    background-color: ${colors.accent_darken};
+    text-decoration: none;
+  }
+`;
+
+const OpenStream = styled.a`
+  background-color: ${colors.accent};
+  border: none;
+  border-radius: ${theme.borderRadius};
+  color: ${colors.text_alt} !important;
+  font-size: ${fontSizes.sm};
+  font-weight: 600;
+  padding: 0.8rem 1.4rem;
+  transition: ${theme.transition};
+
+  &:hover {
+    background-color: ${colors.accent_darken};
+    text-decoration: none;
+  }
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+`;
+
+const StyledLink = styled.a`
+  color: ${colors.text} !important;
+  font-size: 30px;
+  ${media.sm`font-size: 30px;`};
+  ${media.md`font-size: 40px;`};
+  font-weight: 600;
+  text-decoration: none !important;
+  position: relative;
+  margin-bottom: 2.5rem;
+
+  &:before {
+    background-color: ${colors.accent};
+    bottom: 0.15rem;
+    content: "";
+    left: 0;
+    height: 0.25rem;
+    position: absolute;
+    transition: ${theme.transition};
+    width: 0;
+  }
+
+  &:hover:before {
+    width: 100%;
+  }
+`;
+
+export const Up = (props) => {
+  const [closed, setClosed] = React.useState(false);
+
+  return closed ? (
+    <div id="up">
+      <Landing data={props.data.hero} />
       <Line />
-    </div>
-    <Anchor anchor="speakers" />
-    <Speakers speakers={props.data.speakers} />
 
-    <div
-      className="container"
-      style={{ marginTop: "4rem", marginBottom: "6rem" }}
-    >
-      <Line />
-    </div>
-    <Anchor anchor="schedule" />
-    <Schedule data={props.data.schedule} />
+      <Anchor anchor="about" />
+      <About data={props.data.about} />
+      <Countdown
+        target={props.data.countdown.time}
+        location={props.data.countdown.location}
+      />
 
-    <div className="container" style={{ marginTop: "6rem" }}>
-      <Line />
-    </div>
+      <div
+        className="container"
+        style={{ marginTop: "8rem", marginBottom: "6rem" }}
+      >
+        <Line />
+      </div>
+      <Anchor anchor="speakers" />
+      <Speakers speakers={props.data.speakers} />
 
-    <Anchor anchor="register" />
-    <Form />
-  </div>
-);
+      <div
+        className="container"
+        style={{ marginTop: "4rem", marginBottom: "6rem" }}
+      >
+        <Line />
+      </div>
+      <Anchor anchor="schedule" />
+      <Schedule data={props.data.schedule} />
+
+      <div className="container" style={{ marginTop: "6rem" }}>
+        <Line />
+      </div>
+
+      <Anchor anchor="register" />
+      <Form />
+    </div>
+  ) : (
+    <Loader>
+      <h1>SET UP is live!</h1>
+      <StyledLink href="https://www.twitch.tv/setottawa">
+        twitch.tv/setottawa
+      </StyledLink>
+      <ButtonGroup>
+        <BackButton onClick={() => setClosed(true)}>Close</BackButton>
+        <OpenStream href="https://www.twitch.tv/setottawa">
+          Open Stream
+        </OpenStream>
+      </ButtonGroup>
+    </Loader>
+  );
+};
